8f94145d3cf18b6586fdec769d7604246a7914f7
MarounFahed <maroun.fahed6@gmail.com>
2022-03-01 11:43:14 +0200

    Setting up the files

---
+++ TwitterClone/js/index.js
@@ -0,0 +1,19 @@
+const URL = 'http://localhost:3000/tweets'
+
+/** Retrieve Twitter Data from API */
+const getTwitterData = () => {}
+
+/** Save the next page data */
+const saveNextPage = (metadata) => {}
+
+/** Handle when a user click on a trend */
+const selectTrend = (e) => {}
+
+/**Set the visibility of next page based on if there is data on next page */
+const nextPageButtonVisbility = (metadata) => {}
+
+/** Build HTML for Tweets Images */
+const buildImages = (mediaList) => {}
+
+/** Build HTML for Tweets Video */
+const buildVideo = (mediaList) => {}
