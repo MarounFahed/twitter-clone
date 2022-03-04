308b0e4fcfd36dbd7ed39fbc7d51ce418b0e1373
MarounFahed <maroun.fahed6@gmail.com>
2022-03-04 10:52:44 +0200

    Add a URL for API v 1.1

--- Backend/api/helpers/twitter.js
+++ Backend/api/helpers/twitter.js
@@ -1,13 +1,17 @@
 const axios = require('axios')
 
-const URL = 'https://api.twitter.com/2/tweets/search/recent'
+const URLv2 = 'https://api.twitter.com/2/tweets/search/recent'
+const URLv1 = 'https://api.twitter.com/1.1/search/tweets.json'
+
+/** IF URLv2 is being used --> switch the params to "query" and "max_results"
+ * IF URLv1 is being used --> switch the params to "q" and "count" */
 
 class Twitter {
   get(query, max_results) {
-    return axios.get(URL, {
+    return axios.get(URLv1, {
       params: {
-        query: query,
-        max_results: max_results,
+        q: query,
+        count: max_results,
       },
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
